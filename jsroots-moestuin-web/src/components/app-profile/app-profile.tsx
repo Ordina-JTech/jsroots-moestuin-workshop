ba38ac2617e3aa46d40ddeb4e3eca98e315e4873
Kevin Donkers <kevin.donkers@ordina.nl>
2018-11-27 15:08:21 +0100

    Update app profile compared to final

--- jsroots-moestuin-web/src/components/app-profile/app-profile.tsx
+++ jsroots-moestuin-web/src/components/app-profile/app-profile.tsx
@@ -1,3 +1,20 @@
-export class AppProfile {
+import {Component, Prop} from '@stencil/core';
+import {MatchResults} from '@stencil/router';
 
+@Component({
+  tag: 'app-profile',
+  shadow: true
+})
+export class AppProfile {
+  @Prop() match: MatchResults;
+
+  normalize(name: string): string {
+    if (name) {
+      return name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase();
+    }
+    return '';
+  }
+
+  render(): JSX.Element {
+  }
 }
